c10bb3d76182261a8c3a8e4b94a0dc41c6f4e106
bturan27 <begumturann@outlook.com>
2022-08-01 11:34:29 -0700

    adding

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ const App = () => {
   return (
     <div>
         <Intro/>
+
     </div>
   )
 };
--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -1,5 +1,6 @@
 import './intro.css'
 import React from 'react'
+import Me from "../../img/begum.jpeg"
 
 const Intro = () => {
   return (
@@ -11,13 +12,17 @@ const Intro = () => {
           <div className='i-title'>
               <div className='i-title-wrapper'>
                 <div className='i-title-item'> Software Developer</div>
+
               </div>
           </div>
               <div className='i-desc'>
           I came to the US in 2014 as an exchange student and have worked 6 consecutive summers as a waitress in Montana and Wyoming. This allowed me to sponsor myself to further my education in the computer science field. Since the start of obtaining my Master's degree, I have held internships in software development in Chicago and Montana, as well as completed software development bootcamp in Silicon Valley. I am trilingual (English, German & Turkish) and constantly strive to advance these languages as well as programming.
               </div>
           </div>
-          <div className='i-right'>rigth</div>
+          </div>
+          <div className='i-right'>
+            <div className='i-bg'></div>
+            <img src={Me} alt="" className='i-img'></img>
       </div>
     </div>
   )
